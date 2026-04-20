5430db6ea2ddf740e0dba2d4dcfceb8be1282d64
Bohdan Sharubin <Bogdansh13@gmail.com>
2026-04-20 11:54:52 +0300

    extend jwt-token error handling

--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -10,6 +10,12 @@ module.exports = (err, req, res, next) => {
   } else if (err.name == "CastError") {
     statusCode = 400;
     message = "invalid id format";
+  } else if (err.name === "TokenExpiredError") {
+    statusCode = 401;
+    message = "Token has expired. Please log in again";
+  } else if (err.name === "JsonWebTokenError") {
+    statusCode = 401;
+    message = "Invalid token. Please log in again";
   }
   console.error(`${new Date().toISOString()} : ${err}`);
   return res.status(statusCode).json({
