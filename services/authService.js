7003e9cdd60138f3e1183142f08eff0e5a18768d
Bohdan Sharubin <Bogdansh13@gmail.com>
2026-04-20 20:36:40 +0300

    add validation for authentication

--- services/authService.js
+++ services/authService.js
@@ -3,12 +3,6 @@ const User = require("../models/User");
 const AppError = require("../errors/AppError");
 
 exports.registerUser = async ({ name, email, password, confirmPassword }) => {
-  if (!name || !email || !password || !password) {
-    throw AppError.badRequest("All fields are required");
-  } else if (password !== confirmPassword) {
-    throw AppError.badRequest("Passwords are not matching");
-  }
-
   const existingUser = await User.findOne({ email });
   if (existingUser) {
     throw AppError.conflict("User with this email already exists");
@@ -24,10 +18,6 @@ exports.registerUser = async ({ name, email, password, confirmPassword }) => {
 };
 
 exports.loginUser = async ({ email, password }) => {
-  if (!email || !password) {
-    throw AppError.badRequest("Enter email and password");
-  }
-
   const user = await User.findOne({ email }).select("+password");
   if (!user) {
     throw AppError.unauthorized("Wrong email or password");
