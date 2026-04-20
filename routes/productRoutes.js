ca9179af0e1098951334553b3cf7fea32a99073e
Bohdan Sharubin <Bogdansh13@gmail.com>
2026-04-20 16:40:48 +0300

    use validate middleware in product router

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -10,12 +10,17 @@ const {
 } = require("../controllers/productController");
 const protect = require("../middlewares/protect");
 const restrictTo = require("../middlewares/restrictTo");
+const validate = require("../middlewares/validate");
+const {
+  createProductSchema,
+  updateProductSchema,
+} = require("../validators/productValidation");
 
 router.get("/", getAllProducts);
 router.get("/:id", getProduct);
 
-router.post("/", protect, createProduct);
-router.put("/:id", protect, updateProduct);
+router.post("/", protect, validate(createProductSchema), createProduct);
+router.put("/:id", protect, validate(updateProductSchema), updateProduct);
 
 router.delete("/:id", protect, restrictTo("admin"), deleteProduct);
 
