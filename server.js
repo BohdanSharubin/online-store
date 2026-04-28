772d9f6649334ba8a67139167ba24e9d3d953c63
Bohdan Sharubin <Bogdansh13@gmail.com>
2026-04-28 12:08:03 +0300

    Fix  not a constructor and not a function erorrs

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use("/api/products", productRoutes);
 
 app.use(
   asyncHandler(async (req, res) => {
-    throw new AppError.notFound(`Route ${req.originalUrl} not found`);
+    throw AppError.notFound(`Route ${req.originalUrl} not found`);
   }),
 );
 app.use(errorHandler);
