dfea1b3b35ab9d30bd61594804d2aa04977425ec
Bohdan Sharubin <Bogdansh13@gmail.com>
2026-04-29 17:56:25 +0300

    Add middleware for create page with redirection

---
+++ middlewares/protectStatic.js
@@ -0,0 +1,22 @@
+const jwt = require("jsonwebtoken");
+const User = require("../models/User");
+const asyncHandler = require("../middlewares/asyncHandler");
+
+const protectStatic = async (req, res, next) => {
+  const authHeader = req.headers.authorization;
+  if (!authHeader || !authHeader.startsWith("Bearer ")) {
+    return res.redirect("/login.html");
+  }
+
+  const token = authHeader.split(" ")[1];
+  const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+  const user = await User.findById(decoded.id);
+  if (!user) {
+    res.redirect("/login.html");
+  }
+  req.user = user;
+  next();
+};
+
+module.exports = protectStatic;
