2536c250fcffb625ce91f7420878ee14199f6468
Bohdan Sharubin <Bogdansh13@gmail.com>
2026-05-26 18:12:02 +0300

    add route for add-review page

--- routes/clientRoutes.js
+++ routes/clientRoutes.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const router = express.Router();
 const protectStatic = require("../middlewares/protectStatic");
+const controller = require("../controllers/frontendController");
 
-router.get("/create.html", protectStatic, express.static("public"));
-
+router.get("/create", protectStatic, controller.createProduct);
+router.get("/add-review", protectStatic, controller.addReview);
 module.exports = router;
