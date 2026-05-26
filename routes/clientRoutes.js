3ba9336028edd532ef1bceb6311441c3f9abefed
Bohdan Sharubin <Bogdansh13@gmail.com>
2026-05-26 19:59:18 +0300

    add use for all routes in client routes

--- routes/clientRoutes.js
+++ routes/clientRoutes.js
@@ -2,9 +2,12 @@ const express = require("express");
 const router = express.Router();
 const protectStatic = require("../middlewares/protectStatic");
 const controller = require("../controllers/frontendController");
+const path = require("path");
 
 router.get("/", controller.mainPage);
 router.get("/create", protectStatic, controller.createProduct);
 router.get("/add-review", protectStatic, controller.addReview);
-router.get("*", controller.mainPage);
+router.use("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../public/index.html"));
+});
 module.exports = router;
