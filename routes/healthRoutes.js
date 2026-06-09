6a33c2154d640cbb6f98f503504535e5addf09cd
Bohdan Sharubin <sharubin.reserve@gmail.com>
2026-06-09 15:37:09 +0300

    docs: add jsdoc to heath check routes

--- routes/healthRoutes.js
+++ routes/healthRoutes.js
@@ -1,6 +1,52 @@
 const router = require("express").Router();
 const mongoose = require("mongoose");
 
+/**
+ * @swagger
+ * /health:
+ *   get:
+ *     summary: Retrieving information about current server status
+ *     tags: [HealthCheck]
+ *     responses:
+ *       200:
+ *         description: Server is working fine
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 status:
+ *                   type: string
+ *                   example: ok
+ *                 db:
+ *                   type: string
+ *                   example: connected
+ *                 uptime:
+ *                   type: string
+ *                   example: 2s
+ *                 env:
+ *                   type: string
+ *                   example: dev
+ *       503:
+ *         description: Server has problem with db
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 status:
+ *                   type: string
+ *                   example: error
+ *                 db:
+ *                   type: string
+ *                   example: disconnected
+ *                 uptime:
+ *                   type: string
+ *                   example: 2s
+ *                 env:
+ *                   type: string
+ *                   example: dev
+ */
 router.get("/", async (req, res) => {
   const dbOk = mongoose.connection.readyState === 1;
   res.status(dbOk ? 200 : 503).json({
